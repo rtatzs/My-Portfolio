fc3cd3057641dd4a2260a464358c6a3f98d873e8
Rtatzs <kibara.kuro@gmail.com>
2022-03-13 13:19:24 +0800

    xxx

--- app.py
+++ app.py
@@ -5,26 +5,32 @@ from re import I
 from myprofilelib.profile import getInitializeProfile
 from pickle import NONE, TRUE
 from flask import Flask, render_template, request, flash, jsonify
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
+# from google.auth.transport.requests import Request
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from googleapiclient.discovery import build
+# from google.oauth2 import service_account
 
 app = Flask(__name__)
 app.secret_key = "f5bb0c8de146c67b44babbf4e6584cc0"
 
-SERVICE_ACCOUNT_FILE = "static/files/2c9c9cd97be2e079377d9dfb6c1139d8.json"
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-creds = None
-creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-SAMPLE_SPREADSHEET_ID = '1yNF_F7cLnsIWfTJ6gncb9ty_NMo039PStsxPmo2i1oo'
-service = build("sheets", "v4", credentials=creds)
-sheet = service.spreadsheets()
+# SERVICE_ACCOUNT_FILE = "static/files/2c9c9cd97be2e079377d9dfb6c1139d8.json"
+# SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+# creds = None
+# creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# SAMPLE_SPREADSHEET_ID = '1yNF_F7cLnsIWfTJ6gncb9ty_NMo039PStsxPmo2i1oo'
+# service = build("sheets", "v4", credentials=creds)
+# sheet = service.spreadsheets()
 
 @app.route("/MyProfile")
 def myprofile():
 	myprofile = getInitializeProfile()
 	return render_template("myprofile.html", myprofile = myprofile)
+
+@app.route("/MyProfile2")
+def myprofile2():
+	myprofile = getInitializeProfile()
+	return render_template("myprofile.html", myprofile = myprofile)
+
 
 # @app.route("/readsheet")
 # def GoogleSheetRead():
